b33a3dec347e7a1ceaf55dfd89ed1bb89ec57036
NARAHARI, Geethanjali <Geethanjali.NARAHARI@suncorp.com.au>
2017-12-05 23:47:41 +1100

    [DMPM-42] Welcome Screen * Added Test methods related to welcome screen

---
+++ src/test/java/test/marketplace/common/WelcomeTest.java
@@ -0,0 +1,47 @@
+package test.marketplace.common;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+import pages.App;
+
+public class WelcomeTest extends App {
+	
+	@Test (groups = {"DMPM-42", "DMPM-216", "marketplace", "welcome", "priority-minor"})
+	public void testWelcomePageElements(){
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+		Assert.assertNotNull(welcomePage.checkRegisterButton(), "Welcome screen - Register button is not shown");
+		Assert.assertNotNull(welcomePage.checkLoginButton(), "Welcome screen - Login button is not shown");
+		Assert.assertNotNull(welcomePage.checkGuestAccessButton(), "Welcome screen - Guest Access button is not shown");
+		Assert.assertNotNull(welcomePage.checkNeedHelpButton(), "Welcome screen - Need Help button is not shown");
+	}
+	
+	@Test (groups = {"DMPM-42", "DMPM-217", "marketplace", "welcome", "priority-minor"})
+	public void testNavigatingToLoginScreen(){
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+		welcomePage.tapLoginButton();
+		Assert.assertNotNull(loginPage.checkLoginPageTitle(), "Login page - Login Title not shown");
+		loginPage.tapBackButton();
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+	}
+	
+	@Test (groups = {"DMPM-42", "DMPM-218", "marketplace", "welcome", "priority-minor"})
+	public void testNavigatingToRegisterScreen(){
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+		welcomePage.tapRegisterButton();
+		Assert.assertNotNull(registrationPage.checkRegistrationPageTitle(), "Registration page - Register Title not shown");
+		registrationPage.tapCancelButton();
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+	}
+	
+	@Test (groups = {"DMPM-76", "DMPM-324", "marketplace", "Guest user", "priority-minor"})
+	public void testNavigatingToGuestAccessScreen(){
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+		welcomePage.tapGuestAccessButton();
+		// TODO, Once the final guest screen is implemented, this needs to be updated
+		Assert.assertNotNull(guestAccessPage.checkGuestAccessPageTitle(), "Guest Access page - 'Guest Access' Title not shown");
+		guestAccessPage.tapBackButton();
+		Assert.assertNotNull(welcomePage.checkWelcomeSuncorpImage(), "Welcome screen -Suncorp image is not shown");
+	}
+}
