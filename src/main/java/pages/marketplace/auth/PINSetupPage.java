08de1ba6cb92bec5a96598c648c05d0eb97bb740
NARAHARI, Geethanjali <Geethanjali.NARAHARI@suncorp.com.au>
2017-12-05 23:55:27 +1100

    [DMPM-19] PIN Setup * Added test methods for PIN Setup

---
+++ src/main/java/pages/marketplace/auth/PINSetupPage.java
@@ -0,0 +1,87 @@
+package pages.marketplace.auth;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import automation.framework.common.BasePage;
+import io.appium.java_client.AppiumDriver;
+
+public class PINSetupPage extends BasePage {
+	
+	public PINSetupPage(AppiumDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	private By enterPINLabel = By.id("au.com.suncorp.marketplace:id/enterPinMessage");
+	private By reEnterPINLabel = By.id("au.com.suncorp.marketplace:id/reenterPinMessage");
+	private By cancelButton = By.id("au.com.suncorp.marketplace:id/pinSetupCancelButton");
+	
+	private By invalidPINAlertMessage = By.id("android:id/message");
+	private By invalidPINAlertOKButton = By.id("android:id/button1");
+
+	private By pinsDonotMatchErrorMessage = By.id("au.com.suncorp.marketplace:id/pinSetupInlineError");
+	
+	private By pinSuccessfullySetNotification = By.id("au.com.suncorp.marketplace:id/successAlertDialog");
+	private By pinSuccessfullySetMessage = By.id("au.com.suncorp.marketplace:id/dialogMessage");
+	
+	public WebElement checkEnterPINLabel() {
+		return find(enterPINLabel);
+	}
+	
+	public String getEnterPINLabel() {
+		return getText(enterPINLabel);
+	}
+	
+	public WebElement checkReEnterPINLabel() {
+		return find(reEnterPINLabel);
+	}	
+	
+	public String getReEnterPINLabel() {
+		return getText(reEnterPINLabel);
+	}
+	
+	public WebElement checkCancelButton() {
+		return find(cancelButton);
+	}
+	
+	public void tapCancelButton() {
+		tapElement(cancelButton);
+	}
+	
+	public WebElement checkInvalidPINAlertMessage() {
+		return find(invalidPINAlertMessage);
+	}
+	
+	public String getInvalidPINAlertMessage() {
+		return getText(invalidPINAlertMessage);
+	}
+	
+	public WebElement checkInvalidPINAlertOKButton() {
+		return find(invalidPINAlertOKButton);
+	}
+	
+	public void tapInvalidPINAlertOKButton() {
+		tapElement(invalidPINAlertOKButton);
+	}
+	
+	public WebElement checkPINsDonotMatchErrorMessage() {
+		return find(pinsDonotMatchErrorMessage);
+	}
+	
+	public String getPINsDonotMatchErrorMessage() {
+		return getText(pinsDonotMatchErrorMessage);
+	}
+	
+	public WebElement checkPINSuccessfullySetNotification() {
+		return find(pinSuccessfullySetNotification);
+	}
+	
+	public WebElement checkPINSuccessfullySetMessage() {
+		return find(pinSuccessfullySetMessage);
+	}
+	
+	public String getPINSuccessfullySetMessage() {
+		return getText(pinSuccessfullySetMessage);
+	}
+}
---
+++ src/main/java/pages/marketplace/auth/RegistrationPage.java
@@ -0,0 +1,27 @@
+package pages.marketplace.auth;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import automation.framework.common.BasePage;
+import io.appium.java_client.AppiumDriver;
+
+public class RegistrationPage extends BasePage {
+	
+	public RegistrationPage(AppiumDriver driver) {
+		super(driver);
+	}
+	
+	//TODO -> id needs to be added
+	private By registrationPageTitle = By.xpath("//android.widget.TextView[@text='Register for the app']");
+	private By cancelButton = By.id("au.com.suncorp.marketplace:id/cancelButton");
+	
+	public WebElement checkRegistrationPageTitle(){
+		return find(registrationPageTitle,20);
+	}
+	
+	public void tapCancelButton(){
+		tapElement(cancelButton);
+	}
+	
+}
