ba4308a334978f1e6b79f35e31833597b829f31e
GEORGE, Gitin <Gitin.GEORGE@suncorp.com.au>
2018-02-26 11:32:38 +1100

    [TECH] Connecting to StubServer through the Config FIle * Fetch the current IP address and append to Global Base URL * Edit Banking Base URL appropriately

--- src/main/java/pages/marketplace/common/ConfigPage.java
+++ src/main/java/pages/marketplace/common/ConfigPage.java
@@ -1,28 +1,33 @@
 package pages.marketplace.common;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.openqa.selenium.By;
 
 import automation.framework.common.BasePage;
 import io.appium.java_client.AppiumDriver;
 
 public class ConfigPage extends BasePage {
-
+	
 	private By configPageTitle = By.xpath("//android.widget.TextView[@text='Config']");
 	private By globalBaseURL = By.id("au.com.suncorp.marketplace:id/globalBaseUrl");
 	private By applyGlobalBaseUrlButton = By.id("au.com.suncorp.marketplace:id/globalBaseUrlButton");
+	private By bankingBaseUrl = By.id("au.com.suncorp.marketplace:id/bankingBaseUrlEditText");
 	private By continueButton = By.id("au.com.suncorp.marketplace:id/configContinueButton");
 	
 	//FAPI Settings page
 	private By hasBankAccountsToggle = By.id("au.com.suncorp.marketplace:id/hasAccountsToggle");
 	private By someTransactions = By.id("au.com.suncorp.marketplace:id/spendingManyTransactions");
-		
+	
 	public ConfigPage(AppiumDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void dismissConfigPage() {
-		//ConnectToStubSever();
+		
+        //ConnectToStubSever();
+		
 		if(find(configPageTitle,30) != null) {
 			// Added this just to add some delay before checking for keyboard
 			find(continueButton,10);
@@ -38,10 +43,28 @@ public class ConfigPage extends BasePage {
 		}
 	}
 	
-	private void ConnectToStubSever() {
-		tapElement(globalBaseURL);
-		typeValue("192.168.213.31:4567", globalBaseURL);
-		tapElement(applyGlobalBaseUrlButton);
+
+	// Added on 08/02/2018 - GG
+	public void ConnectToStubSever() {
+		InetAddress IP = null;
+		try {
+			IP = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		if(find(globalBaseURL,30) != null) {
+			clearValue(globalBaseURL);
+			typeValue(IP.getHostAddress()+":4567", globalBaseURL);
+			tapElement(applyGlobalBaseUrlButton);
+		}
+		
+		if(find(bankingBaseUrl, 30) != null) {
+			String baseURL = "http://"+IP.getHostAddress()+":4567"+"/marketplace/api/";
+			clearValue(bankingBaseUrl);
+			typeValue(baseURL, bankingBaseUrl);
+		}
+		
 	}
 	
 	public void enableHasBankAccountsToggle() {
