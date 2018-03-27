1719f7104f9ecf2f16b6f08ee3193ca2b7583ef1
KUMAR, Sathish <sathish.kumar@suncorp.com.au>
2018-03-27 14:06:31 +1100

    PR review comments fixed

--- src/test/java/test/marketplace/home/HomeJourneyMenuTest.java
+++ src/test/java/test/marketplace/home/HomeJourneyMenuTest.java
@@ -9,28 +9,22 @@ import pages.marketplace.common.NavigationMenuPage;
 
 public class HomeJourneyMenuTest extends App {
 	
-	@Test (groups = {"DMPM-797", "DMPM-855", "marketplace", "Home buying journey", "priority-minor"})
+	@Test (groups = {"DMPM-797", "DMPM-855", "DMPM-856", "marketplace", "Home buying journey", "priority-minor"})
 	
-	//DMPM-797 - Scenario 1
+	//DMPM-797 - Scenario 1 & 2
 	// Navigating to Home journey option in Menu
+	// Navigating to Home journey screen
 	public void testHomeJourneyMenu()
 	{
 		navigateToHomeJourneyMenu();
 		Assert.assertNotNull(navigationMenu.checkHomeJourneyMenuItem(), " Navigation Draw - Home Journey menu option not shown");
-	}
-	
-	@Test (groups = {"DMPM-797", "DMPM-856", "marketplace", "Home buying journey", "priority-minor"})
-	
-	//DMPM-797 - Scenario 2
-	// Navigating to Home journey screen
-	
-	public void testHomeJourneyScreen()
-	{
-		navigateToHomeJourneyMenu();
 		navigationMenu.tapHomeJourneyMenuItem();
 		Assert.assertNotNull(homeJourneyPage.checkHomeJourneyPageTitle(), "Home Journey Page - Home Journey page title is not shown");
 		Assert.assertNotNull(navigationMenu.checkSplitMenuIcon(), "Home Journey Page - Hamburger Menu not shown");
+		
 	}
+	
+
 
 private void navigateToHomeJourneyMenu() {
 	welcomePage.tapGuestAccessButton();
