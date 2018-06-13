380d9afd13101ad855726af76588276348b5e131
Basavaraj Biradar <basavaraj.biradar@suncorp.com.au>
2018-06-13 15:15:26 +1000

    Offers tests error fixes and copy updates

--- src/test/java/test/marketplace/offers/OffersVehiclesTest.java
+++ src/test/java/test/marketplace/offers/OffersVehiclesTest.java
@@ -3,32 +3,35 @@ package test.marketplace.offers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import automation.framework.common.TestDetails;
 import pages.App;
 
 public class OffersVehiclesTest extends App {
 	// scenario 1
 	// Story DMPM-2253 TCs "DMPM-4649","DMPM-4650"
-	@Test(groups = { "DMPM-814", "DMPM-926","DMPM-2253", "DMPM-4649","DMPM-4650", "marketplace", "Offers", "priority-minor" })
+	@TestDetails(story1="DMPM-814:DMPM-926,DMPM-935", story2="DMPM-2253:DMPM-4649,DMPM-4650")
+	@Test(groups = {"marketplace", "Offers", "priority-minor" })
 	public void testVehicleJourneyOffers() {
-		navigateToVehicleScreen();
+		navigateToVehicleScreen("offers");
 		Assert.assertNotNull(offersPage.checkOfferTitle(), "Vehicle tab Offers - No offer Title");
 		Assert.assertNotNull(offersPage.checkCallToActionButton(), "Vehicle tab Offers - No Action Button");
 		Assert.assertNotNull(offersPage.checkBackgroundImage(), "Vehicle tab Offers - No Background Image");
 		Assert.assertNull(offersPage.checkOfferIcon(), "Vehicle tab Offers - Offer Icon is still present");
 		Assert.assertEquals(offersPage.getOffersDescriptionText(),
-				utils.readTestData("copy", "offersPage", "vehiclesTab", "offerDescription"),
+				utils.readTestData("offersPage", "vehiclesTab", "offerDescription"),
 				"offer description for vehicles tab is not shown as expected");
 		Assert.assertEquals(offersPage.getOffersPageTitle(),
-				utils.readTestData("copy", "offersPage", "vehiclesTab", "offerTitle"),
+				utils.readTestData("offersPage", "vehiclesTab", "offerTitle"),
 				"offer title for vehicles tab is not shown as expected");
 		Assert.assertNotNull(offersPage.checkOfferLogoImage(), "Property Page Offers - No offer logo image");
 		
 	}
 
 	// scenario 2
-	@Test(groups = { "DMPM-814", "DMPM-927", "marketplace", "Offers", "priority-minor" })
+	@TestDetails(story1="DMPM-814:DMPM-927")
+	@Test(groups = {"marketplace", "Offers", "priority-minor" })
 	public void testRetryContentRetrievalVehiclesTab() {
-		navigateToVehicleScreen();
+		navigateToVehicleScreen("offers");
 		if (!(offersPage.checkOfferTitle().isDisplayed() && offersPage.checkCallToActionButton().isDisplayed())) {
 				//&& offersPage.checkBackgroundImage().isDisplayed())) {
 			Assert.assertNotNull(landingPage.checkHomeTab(), " Offers flow - Navigation to different tab failed");
@@ -37,14 +40,20 @@ public class OffersVehiclesTest extends App {
 					" Offers flow - Navigation back to Vehicles tab failed");
 			landingPage.tapVehiclesTab();
 		}
-		Assert.assertNotNull(offersPage.checkOfferIcon(), "Vehicles Page Offers - No offer Image");
+		//Offers icon is removed as per DMPM-2253
+		//Assert.assertNotNull(offersPage.checkOfferIcon(), "Vehicles Page Offers - No offer Image");
 		Assert.assertNotNull(offersPage.checkOfferTitle(), "Vehicle tab Offers - No offer Title");
 		Assert.assertNotNull(offersPage.checkCallToActionButton(), "Vehicle tab Offers - No Action Button");
-		//Assert.assertNotNull(offersPage.checkBackgroundImage(), "Vehicle tab Offers - No Background Image");
+		Assert.assertNotNull(offersPage.checkBackgroundImage(), "Vehicle tab Offers - No Background Image");
 	}
 
-	private void navigateToVehicleScreen() {
-		welcomePage.tapGuestAccessButton();
+	private void navigateToVehicleScreen(String loginType) {
+		if(loginType.equals("offers")) {
+			loginToApp(utils.readTestData("offersPage", "login","email"), utils.readTestData("offersPage", "login","pwd"));
+			}
+		else {
+			welcomePage.tapGuestAccessButton();
+		}
 		Assert.assertNotNull(landingPage.checkVehiclesTab(), " Offers flow - Vehicles tab not shown");
 		landingPage.tapVehiclesTab();
 		offersPage.scrollToOffersBanner();
--- src/test/java/test/marketplace/offers/OffersWealthTest.java
+++ src/test/java/test/marketplace/offers/OffersWealthTest.java
@@ -3,32 +3,33 @@ package test.marketplace.offers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import automation.framework.common.TestDetails;
 import pages.App;
 
 public class OffersWealthTest extends App {
 	// scenario 1
 	// Story DMPM-2253 TCs "DMPM-4649","DMPM-4650"
-	@Test(groups = { "DMPM-816", "DMPM-930","DMPM-2253", "DMPM-4649","DMPM-4650", "marketplace", "Offers", "priority-minor" })
+	@TestDetails(story1="DMPM-816:DMPM-930", story2="DMPM-2253:DMPM-4649,DMPM-4650")
+	@Test(groups = {"marketplace", "Offers", "priority-minor" })
 	public void testWealthJourneyOffers() {
-		navigateToWealthScreen();
+		navigateToWealthScreen("offers");
 		Assert.assertNotNull(offersPage.checkOfferTitle(), "Wealth tab Offers - No offer Title");
 		Assert.assertNotNull(offersPage.checkCallToActionButton(), "Wealth tab Offers - No Action Button");
 		Assert.assertNotNull(offersPage.checkBackgroundImage(), "Wealth tab Offers - No Background Image");
 		Assert.assertNull(offersPage.checkOfferIcon(), "Wealth tab Offers - Offer Icon is still present");
 		Assert.assertEquals(offersPage.getOffersDescriptionText(),
-				utils.readTestData("copy", "offersPage", "wealthTab", "offerDescription"),
-				"offer description for Wealth tab is not shown as expected");
+				utils.readTestData("offersPage", "wealthTab", "offerDescription"), "offer description for Wealth tab is not shown as expected");
 		Assert.assertEquals(offersPage.getOffersPageTitle(),
-				utils.readTestData("copy", "offersPage", "wealthTab", "offerTitle"),
-				"offer title for Wealth tab is not shown as expected");
+				utils.readTestData("offersPage", "wealthTab", "offerTitle"), "offer title for Wealth tab is not shown as expected");
 		Assert.assertNotNull(offersPage.checkOfferLogoImage(), "Property Page Offers - No offer logo image");
 		
 	}
 
 	// scenario 2
-	@Test(groups = { "DMPM-816", "DMPM-931", "marketplace", "Offers", "priority-minor" })
+	@TestDetails(story1="DMPM-816:DMPM-931")
+	@Test(groups = {"marketplace", "Offers", "priority-minor" })
 	public void testRetryContentRetrievalWealthTab() {
-		navigateToWealthScreen();
+		navigateToWealthScreen("offers");
 		if (!(offersPage.checkOfferTitle().isDisplayed() && offersPage.checkCallToActionButton().isDisplayed())) {
 				//&& offersPage.checkBackgroundImage().isDisplayed())) {
 			Assert.assertNotNull(landingPage.checkHomeTab(), " Offers flow - Navigation to different tab failed");
@@ -36,14 +37,20 @@ public class OffersWealthTest extends App {
 			Assert.assertNotNull(landingPage.checkFinanceTab(), " Offers flow - Navigation back to Vehicles tab failed");
 			landingPage.tapFinanceTab();
 		}
-		Assert.assertNotNull(offersPage.checkOfferIcon(), "Wealth Page Offers - No offer Image");
+		//Offers icon is removed as per DMPM-2253
+		//Assert.assertNotNull(offersPage.checkOfferIcon(), "Wealth Page Offers - No offer Image");
 		Assert.assertNotNull(offersPage.checkOfferTitle(), "Wealth tab Offers - No offer Title");
 		Assert.assertNotNull(offersPage.checkCallToActionButton(), "Wealth tab Offers - No Action Button");
-		//Assert.assertNotNull(offersPage.checkBackgroundImage(), "Wealth tab Offers - No Background Image");
+		Assert.assertNotNull(offersPage.checkBackgroundImage(), "Wealth tab Offers - No Background Image");
 	}
 
-	private void navigateToWealthScreen() {
-		welcomePage.tapGuestAccessButton();
+	private void navigateToWealthScreen(String loginType) {
+		if(loginType.equals("offers")) {
+			loginToApp(utils.readTestData("offersPage", "login","email"), utils.readTestData("offersPage", "login","pwd"));
+			}
+		else {
+			welcomePage.tapGuestAccessButton();
+		}
 		Assert.assertNotNull(landingPage.checkFinanceTab(), " Offers flow - Wealths tab not shown");
 		landingPage.tapFinanceTab();
 		offersPage.scrollToOffersBanner();
